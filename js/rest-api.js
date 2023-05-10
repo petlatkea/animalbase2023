fe7ff44ad0c64f1ecddb400cc4ad0f861f2a7a66
Peter Lind (@KEA) <petl@kea.dk>
2023-05-10 17:45:56 +0200

    added performance-measurements

--- js/rest-api.js
+++ js/rest-api.js
@@ -1,9 +1,13 @@
 const endPoint = "https://animalbase2023-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function getAllAnimals() {
+  const start = performance.now();
   const response = await fetch(endPoint+"/animals.json");
   const originalJson = await response.json();
   const data = prepareData(originalJson);
+  const end = performance.now();
+
+  console.log("get took: " + (end-start) + " milliseconds");
 
   return data;
 }
