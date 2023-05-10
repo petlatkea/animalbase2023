878fc0e91c7be604cdcd918bcca9743d6cc64ea9
Peter Lind (KEA) <petl@kea.dk>
2023-05-10 08:17:59 +0200

    test-fetch to get up and running

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 "use strict";
 
+const endPoint = "https://animalbase2023-default-rtdb.europe-west1.firebasedatabase.app/";
+
 window.addEventListener("load", start);
 
-function start() {
+async function start() {
   console.log("JavaScript is running");
+
+  const animals = await getAllAnimals();
+
+  console.log(animals);
+}
+
+async function getAllAnimals() {
+  const response = await fetch(endPoint+"/animals.json");
+  const originalJson = await response.json();
+  const data = prepareData(originalJson);
+
+  return data;
+}
+
+function prepareData(listOfObjects) {
+  const arrayFromObjects = [];
+  
+  for (const object in listOfObjects) {
+    const post = listOfObjects[object];
+    post.id = object;
+    arrayFromObjects.push(post);
+  }
+  return arrayFromObjects;
 }
