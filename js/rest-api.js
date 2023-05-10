7b5b665cb8da825f618254ae7bd714bb7cb5a562
Peter Lind (@KEA) <petl@kea.dk>
2023-05-10 17:49:48 +0200

    added caching - including performance measurement

--- js/rest-api.js
+++ js/rest-api.js
@@ -1,15 +1,29 @@
 const endPoint = "https://animalbase2023-default-rtdb.europe-west1.firebasedatabase.app/";
 
+let allAnimals = [];
+let lastFetch = 0;
+
 async function getAllAnimals() {
   const start = performance.now();
-  const response = await fetch(endPoint+"/animals.json");
-  const originalJson = await response.json();
-  const data = prepareData(originalJson);
+
+  const now = Date.now();
+  const timePassedSinceLastFetch = now - lastFetch;
+  // Only fetch if more than 10 seconds has passed since last fetch
+  if( timePassedSinceLastFetch > 10_000) {  // <- hardcoded time - should maybe be something different
+    await refetchAllAnimals();
+  }
   const end = performance.now();
 
-  console.log("get took: " + (end-start) + " milliseconds");
+  console.log("optimized get took: " + (end-start) + " milliseconds");
 
-  return data;
+  return allAnimals;
+}
+
+async function refetchAllAnimals() {
+  const response = await fetch(endPoint+"/animals.json");
+  const originalJson = await response.json();
+  allAnimals = prepareData(originalJson);
+  lastFetch = Date.now();
 }
 
 function prepareData(listOfObjects) {
