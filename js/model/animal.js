ca82dae915694fcdd28d1995b067e8589f4a0be3
Peter Lind (@KEA) <petl@kea.dk>
2023-10-09 17:05:49 +0200

    Whoops - forgot about id!

--- js/model/animal.js
+++ js/model/animal.js
@@ -1,10 +1,13 @@
 export default class Animal {
   constructor(obj) {
+    this.id = obj.id;
     this.name = obj.name;
     this.desc = obj.desc;
     this.type = obj.type;
     this.age  = obj.age;
     this.star = obj.star;
     this.winner = obj.winner;
+
+    Object.defineProperty(this, "id", {writable: false});
   }
 }
